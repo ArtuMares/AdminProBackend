dbad8e3c6f01b2f543176cf2719c6b495c538c9c
Arturo Mares Sánchez <artumares08@gmail.com>
2022-07-05 13:36:33 -0500

    Correccion de errores en endpoints de Auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,18 +13,18 @@ const login = async (req, res = response) => {
         const usuarioDB = await Usuario.findOne({email});
         //Verificar email 
         if (!usuarioDB){
-            res.status(402).json({
+            return res.status(402).json({
                 ok:false,
                 msg: "email no encontrado"
-            })
+            });
         }
         //Verificar contraseña 
         const validPassword = bcrypt.compareSync(password, usuarioDB.password);
         if (!validPassword){
-            res.status(402).json({
+            return res.status(402).json({
                 ok: false,
                 msg: "La contraseña no es válida"
-            })
+            });
         }
         //Generar JWT
         const token = await generarJWT(usuarioDB.id);
@@ -33,8 +33,8 @@ const login = async (req, res = response) => {
             ok: true,
             token
         });
-    } catch (error) {
-        console.log(error);
+    } catch (err) {
+        console.log(err);
         res.status(500).json({
             ok: false,
             msg: "Error inesperado"
--- routes/auth.js
+++ routes/auth.js
@@ -30,7 +30,7 @@ router.post("/google",
     ]
     , googleSignIn);
 
-//*Login con google
+//*renovar roken
 router.get("/renew",
 [
     validarJWT
